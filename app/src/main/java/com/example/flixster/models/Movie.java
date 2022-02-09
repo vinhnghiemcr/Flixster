6f6ba6ae016d8bbd1313611c33b41aa22346ef7e
Nghiem Truong <vinhnghiemcr@gmail.com>
2022-02-08 23:20:32 -0600

    Create Movie model
    Movie model has a method which will take a movie  json array and return a list of movie

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -1,4 +1,29 @@
 package com.example.flixster.models;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Movie {
+
+    String backdrop_path;
+    String original_title;
+    String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        backdrop_path = jsonObject.getString("backdrop_path");
+        original_title = jsonObject.getString("original_title");
+        overview = jsonObject.getString("overview");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i=0; i<movieJsonArray.length(); i++) {
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
 }
